d4d6b5ce3460a39ae17a5e76e5a339d0000bf043
pressstart101 <walterscott404@yandex.com>
2016-06-01 11:06:12 -0400

    done

--- ios-prework-objc-assessment/AppDelegate.m
+++ ios-prework-objc-assessment/AppDelegate.m
@@ -16,7 +16,49 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    NSMutableArray *numbersArray = [[NSMutableArray alloc]init];
+    numbersArray = [[self generateArrayOfNumbers]mutableCopy];
+    
+    NSMutableArray *lettersArray = [[NSMutableArray alloc]init];
+    lettersArray = [[self generateArrayOfLetters]mutableCopy];
+    
+    NSMutableDictionary *alphabetDictionary = [[NSMutableDictionary alloc]init];
+    alphabetDictionary = [[self generateNumbersAndLettersDictionaryWithNumbers:numbersArray letters:lettersArray]mutableCopy];
+    
+    numbersArray = [[alphabetDictionary allKeys] mutableCopy];
+    NSMutableArray *mutableLettersArray = [lettersArray mutableCopy];
+    [mutableLettersArray removeAllObjects];
+    mutableLettersArray = [[alphabetDictionary allValues]mutableCopy];
+    NSLog(@"%@ \n %@", numbersArray, mutableLettersArray);
+    
+    NSLog(@"%@", alphabetDictionary);
     return YES;
+}
+-(NSArray *)generateArrayOfNumbers{
+    NSMutableArray *numbers = [[NSMutableArray alloc]init];
+    for(NSUInteger i = 1; i <=26; i++){
+        NSNumber *result = @(i);
+        [numbers addObject:result];
+    }
+    return numbers;
+}
+
+-(NSArray *)generateArrayOfLetters{
+    NSMutableArray *letters = [[NSMutableArray alloc]init];
+    for (char i = 'a'; i <= 'z'; i++) {
+     NSString *result = [NSString stringWithFormat:@"%c", i];
+        [letters addObject:result];
+      
+    }
+    return letters;
+}
+-(NSDictionary *)generateNumbersAndLettersDictionaryWithNumbers:(NSArray *)numbersArray
+                                                        letters:(NSArray *)lettersArray{
+    NSMutableDictionary *resultDictionary =[[NSMutableDictionary alloc]init];
+    for (NSUInteger i = 0; i < [numbersArray count]; i++){
+    resultDictionary[numbersArray[i]] = lettersArray[i];
+    }
+    return resultDictionary;
 }
 
 @end
